9c2263d13bc7844f3672ff7906eaf7beb0ebdbdd
Dan Gallagher <dagallagher221@gmail.com>
2019-09-08 18:07:48 -0400

    added basic game javascript, still having issues getting the game to work correctly

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//Begin by identifying variables needed for the game
+    var winCount = 0;
+    var lossCount = 0;
+    var guessesRemaining=9;
+    var lettersGuessed = [];
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Create a way for the computer to select a letter at random
+var randomNum = Math.floor(Math.random()*alphabet.length);
+var compChoice = alphabet[randomNum];
+console.log(compChoice);
+
+//Add event listener for user input
+document.onkeyup = function(event) {
+    var userChoice = event.key;
+
+    //Compare the User selection to the Computer selection
+    if (compChoice === userChoice) {
+        alert("You win! Congratulations!");
+        document.getElementById("winCount").innerHTML = winCount++;
+        lettersGuessed= [];
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+        var randomNum = Math.floor(Math.random()*alphabet.length);
+        var compChoice = alphabet[randomNum];
+        guessesRemaining=9;
+        document.getElementById("guessesRemaining").innerHTML=9;
+        console.log(compChoice);
+
+    }else{
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining--;
+        lettersGuessed.push(userChoice);
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+    }
+    //Have computer select new letter if the user loses
+    if (guessesRemaining === 0) {
+        lossCount++;
+        document.getElementById("lossCount").innerHTML = lossCount++;
+        alert("You Lost! Try Again")
+        guessesRemaining=9;
+        lettersGuessed=[];
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+        document.getElementById("guessesRemaining").innerHTML = 9;
+        randomNum = Math.floor(Math.random()*alphabet.length);
+        compChoice  = alphabet[randomNum];
+        console.log(compChoice);
+
+
+    }
+
+}
